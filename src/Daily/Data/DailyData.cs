aab2239b4cbcbb68999450a7ea61ef3d94a98285
William-io <capuletos@live.com>
2021-09-26 21:25:00 -0300

    DataBase Created

---
+++ src/Daily/Data/DailyData.cs
@@ -0,0 +1,14 @@
+﻿using Daily.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Daily.Data
+{
+    public class DailyData : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.db;Cache=Shared");
+
+        public DbSet<DailyAnnotation> Annotations { get; set; }
+
+    }
+}
---
+++ src/Daily/Migrations/20210927002233_InitialCreation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Daily.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Annotations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Done = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Annotations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Annotations");
+        }
+    }
+}
