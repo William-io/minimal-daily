59876e63aeb7db9b5e15111f591a04e5a6ffc31d
William-io <capuletos@live.com>
2021-09-26 21:56:55 -0300

    Daily.View of Flunt Validation

---
+++ src/Daily/View/DailyView.cs
@@ -0,0 +1,26 @@
+﻿using Daily.Entities;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Daily.View
+{
+    public class DailyView : Notifiable<Notification>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DailyAnnotation MapTo()
+        {
+
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsNotNull(Title, "Informe o titulo da daily de hoje!")
+                .IsGreaterThan(Title, 5, "O titulo da daily deve conter mais de 5 caracteres")
+                .IsNotNull(Description, "Informe qual é a produção")
+                .IsGreaterThan(Title, 5, "O titulo da daily deve conter mais de 5 caracteres"));
+
+            return new DailyAnnotation(Guid.NewGuid(), Title, Description, false);
+        }
+
+    }
+}
