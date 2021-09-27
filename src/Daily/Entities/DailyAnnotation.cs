709f8fe1a7d696172bbed18851df3b4ecf542bb0
William-io <capuletos@live.com>
2021-09-26 21:01:42 -0300

    Return typing using MapGet

---
+++ src/Daily/Entities/DailyAnnotation.cs
@@ -0,0 +1,4 @@
+﻿namespace Daily.Entities
+{
+    public record DailyAnnotation(Guid Id, string Title, string Description, bool Done);
+}
--- src/Daily/Program.cs
+++ src/Daily/Program.cs
@@ -1,6 +1,8 @@
+using Daily.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => new DailyAnnotation(Guid.NewGuid(), "Wipro Event One", "Terminar deploy", false));
 
 app.Run();
