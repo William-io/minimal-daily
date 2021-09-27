02f783fbb83156fe1943a912daf0288471a556ff
William-io <capuletos@live.com>
2021-09-27 10:53:13 -0300

    MapPost completed,

--- src/Daily/Program.cs
+++ src/Daily/Program.cs
@@ -1,9 +1,13 @@
 using Daily.Data;
-using Daily.Entities;
+using Daily.View;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DailyData>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -16,6 +20,22 @@ app.MapGet("/v1/annotations", (DailyData data) =>
 {
     var annotations = data.Annotations;
     return annotations is not null ? Results.Ok(annotations) : Results.NotFound();
+}).Produces<Annotation>();
+
+app.MapPost("/v1/annotations", (DailyData dataContext, DailyView viewModel) =>
+{
+    var daily = viewModel.MapTo();
+    if (!viewModel.IsValid)
+        return Results.BadRequest(viewModel.Notifications);
+
+    dataContext.Annotations.Add(daily);
+    dataContext.SaveChanges();
+
+    return Results.Created($"/v1/annotatios/{daily.Id}", daily);
 });
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.Run();
